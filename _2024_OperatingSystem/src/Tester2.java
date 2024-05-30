7aae9e465e5cab32cc9fa895aa348a8d0e59ca46
patternplane <bss011202@naver.com>
2024-05-31 02:34:54 +0900

    [JAVA][FEAT] 테스트 환경 개선

--- _2024_OperatingSystem/src/Tester2.java
+++ _2024_OperatingSystem/src/Tester2.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.Scanner;
 
 public class Tester2 {
 	public void Test() {
@@ -8,18 +9,54 @@ public class Tester2 {
 		ArrayList<Process>[] jobsData = ts.readFilesFromDialog();
 		
 		EnumSet<PrintType> printType = EnumSet.of(PrintType.SUMMARY_INFO);
-		
-		int timeSlice1 = 800;
-		int timeSlice2 = 12;
+
+		// Time Slice
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Time Slice Size :");
+        int timeSlice = scanner.nextInt();
+        scanner.close();
 		
 		int currentFile = 1;
+		ArrayList<Result> r;
 		for (ArrayList<Process> jobs : jobsData) {
-			ArrayList<Result> r = ts.getPerProcessResult(NewScheduling.Run(jobs, timeSlice2));
+			System.out.println("File : " + currentFile);
+			
+			System.out.println("FCFS Avg & StDev of Waiting Times : ");
+			r = ts.getPerProcessResult(FCFS.Run(jobs));
+			System.out.print(ts.getAverageWaitingTime(r));
+			System.out.print("\t");
+			System.out.println(ts.getSTDEVWaitingTime(r));
+
+			System.out.println("SJF Avg & StDev of Waiting Times : ");
+			r = ts.getPerProcessResult(SJF.Run(jobs));
+			System.out.print(ts.getAverageWaitingTime(r));
+			System.out.print("\t");
+			System.out.println(ts.getSTDEVWaitingTime(r));
+
+			System.out.println("Priority(P) Avg & StDev of Waiting Times : ");
+			r = ts.getPerProcessResult(PriorityScheduling.Run(jobs, true));
 			System.out.print(ts.getAverageWaitingTime(r));
 			System.out.print("\t");
 			System.out.println(ts.getSTDEVWaitingTime(r));
 			
-			//System.out.println("\n\nFile : " + currentFile);
+			System.out.println("Priority(NP) Avg & StDev of Waiting Times : ");
+			r = ts.getPerProcessResult(PriorityScheduling.Run(jobs, false));
+			System.out.print(ts.getAverageWaitingTime(r));
+			System.out.print("\t");
+			System.out.println(ts.getSTDEVWaitingTime(r));
+			
+			System.out.println("RR Avg & StDev of Waiting Times : ");
+			r = ts.getPerProcessResult(RoundRobin.Run(jobs, timeSlice));
+			System.out.print(ts.getAverageWaitingTime(r));
+			System.out.print("\t");
+			System.out.println(ts.getSTDEVWaitingTime(r));
+			
+			System.out.println("Hybrid SJF RR Avg & StDev of Waiting Times : ");
+			r = ts.getPerProcessResult(NewScheduling.Run(jobs, timeSlice));
+			System.out.print(ts.getAverageWaitingTime(r));
+			System.out.print("\t");
+			System.out.println(ts.getSTDEVWaitingTime(r));
+			
 			
 			//ts.outToFile(".\\result"+currentFile+"_FCFS.txt", ts.getPerProcessResult(ts.printResult(jobs.size(), printType, "FCFS" , FCFS.Run(jobs))));
 			//ts.outToFile(".\\result"+currentFile+"_SJF.txt", ts.getPerProcessResult(ts.printResult(jobs.size(), printType, "SJF" , SJF.Run(jobs))));
